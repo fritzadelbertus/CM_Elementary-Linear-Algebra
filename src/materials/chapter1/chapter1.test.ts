5da49cb2afc1d90cd9cbfadc1570d3deaefa1b38
Fritz <furitsucode@gmail.com>
2022-11-20 18:59:39 +0700

    updated chapter1.ts and chapter1.test.ts
    added Matrix Addition and Subtraction
    added Matrix Transpose
    added Matrix Multiplication
    added Matrix Trace

--- src/materials/chapter1/chapter1.test.ts
+++ src/materials/chapter1/chapter1.test.ts
@@ -1,13 +1,64 @@
 import ChapterOne from "./chapter1";
 
-test('Solved a system of linear equation using Gauss Jordan Elimination', () => {
-    expect(ChapterOne.gaussJordanElim([[1,2,3,5],[5,6,7,8],[9,10,13,12]])).toStrictEqual([-3,3.25,0.5])
+test('The Gauss Jordan Elimination should return a correct solution or an error', () => {
+	expect(ChapterOne.gaussJordanElim([[1,2,3,5],[5,6,7,8],[9,10,13,12]])).toStrictEqual([-3,3.25,0.5])
+	expect(ChapterOne.gaussJordanElim([[1,2,3,4,5],[5,6,7,5,8],[9,10,13,6,12]]))
+		.toStrictEqual('This System of Linear Equation either have no solution or have infinite solutions')
 })
 
-test('Operated matrix multiplication with scalar', () => {
-    expect(ChapterOne.matrixScalarMultiply([[1,2,3,5],[5,6,7,8],[9,10,13,12]], 5)).toStrictEqual([
-        [5,10,15,25],
-        [25,30,35,40],
-        [45,50,65,60]
-    ])
+test('The Matrix Scalar Multiplication should return a scalar multiple of the matrix', () => {
+  expect(ChapterOne.matrixScalarMultiply(
+		[[1,2,3,5],[5,6,7,8],[9,10,13,12]], 5))
+		.toStrictEqual(
+			[[5,10,15,25],[25,30,35,40],[45,50,65,60]]
+		)
+})
+
+test('Matrix Addition and Subtraction should return the correct solution or an error', () => {
+  expect(ChapterOne.matrixAddition(
+    [[1,1,1],[1,1,1],[1,1,1]], 
+    [[1,1,1],[1,1,1],[1,1,1]], true))
+		.toStrictEqual(
+    	[[2,2,2],[2,2,2],[2,2,2]]
+		)
+	expect(ChapterOne.matrixAddition(
+    [[1,1,1],[1,1,1],[1,1,1]], 
+    [[1,1,1],[1,1,1],[1,1,1]], false))
+		.toStrictEqual(
+    	[[0,0,0],[0,0,0],[0,0,0]]
+		)
+	expect(ChapterOne.matrixAddition(
+    [[1,1,1],[1,1,1],[1,1,1]], 
+    [[1,1,1],[1,1,1]], true))
+		.toStrictEqual('Invalid Operation!')
+})
+
+test('Matrix Tranpose should flip the matrix', () => {
+	expect(ChapterOne.matrixTranspose(
+		[[1,2,3],[4,5,6],[7,8,9]]
+	)).toStrictEqual(
+		[[1,4,7],[2,5,8],[3,6,9]]
+	)
+})
+
+test('Matrix Multiplication should return the correct solution or an error', () => {
+	expect(ChapterOne.matrixMultiplication(
+		[[1,2,4], [2,6,0]],
+		[[4,1,4,3],[0,-1,3,1],[2,7,5,2]]
+	)).toStrictEqual(
+		[[12,27,30,13],[8,-4,26,12]]
+	)
+	expect(ChapterOne.matrixMultiplication(
+		[[1,2,4,5], [2,6,0,6]],
+		[[4,1,4,3],[0,-1,3,1],[2,7,5,2]]
+	)).toStrictEqual('Invalid Operation!')
+})
+
+test('Matrix Trace should return the correct solution or an error', () => {
+	expect(ChapterOne.matrixTrace(
+		[[-1,2,7,0],[3,5,-8,4],[1,2,7,-3],[4,-2,1,0]]
+	)).toStrictEqual(11)
+	expect(ChapterOne.matrixTrace(
+		[[-1,2,7,0],[3,5,-8,4],[1,2,7,-3],[4,-2,1,0],[4,3,2,1]]
+	)).toStrictEqual('No Solution. The matrix do not have a main diagonal')
 })
--- src/materials/chapter1/chapter1.ts
+++ src/materials/chapter1/chapter1.ts
@@ -3,39 +3,100 @@
 
 class ChapterOne {
     
-    // Gauss Jordan Elimination
-    static gaussJordanElim( linsys : number[][] ) {
-        const numOfEquation = linsys.length
-        const numOfUnknown = linsys[0].length - 1
-        if (numOfUnknown > numOfEquation) return "This System of Linear Equation either have no solution or have infinite solutions"
-        
-        let input = linsys.slice()
-        const length = input.length
-        for (let i = 0; i < length; i += 1) {
-            if (input[i][i] === 0) {
-                return "Mathematical Error! Cannot divide by zero"
-            }
-            for (let j = 0; j < length; j += 1) {
-                if (i !== j) {
-                    const ratio = input[j][i] / input[i][i]
-                    for (let k = 0; k < length + 1; k += 1) {
-                        input[j][k] = input[j][k] - ratio * input[i][k]
-                    }
-                }
-            }
+  // Gauss Jordan Elimination
+  // Notes: Only solver system of linear equation with one solution
+  static gaussJordanElim( linsys : number[][] ) {
+    const numOfEquation = linsys.length
+    const numOfUnknown = linsys[0].length - 1
+    if (numOfUnknown > numOfEquation) return 'This System of Linear Equation either have no solution or have infinite solutions'
+  
+    let input = linsys.slice()
+    const length = input.length
+    // pointer = i, row to operate = j, column to operate = k
+    for (let i = 0; i < length; i += 1) {
+      if (input[i][i] === 0) return 'Mathematical Error! Cannot divide by zero'
+      for (let j = 0; j < length; j += 1) {
+        if (i !== j) {
+          const ratio = input[j][i] / input[i][i]
+          for (let k = 0; k < length + 1; k += 1) {
+            input[j][k] = input[j][k] - ratio * input[i][k]
+          }
         }
-
-        const output = input.map((row, pointer) => row[length] / row[pointer])
-        return output
+      }
     }
 
-    // Scalar Multiple
-    static matrixScalarMultiply( matrix: number[][], scalar: number) {
-        const input = matrix.slice()
-        const output = input.map((row) => row.map((cell) => cell * scalar))
-        return output
-    }
+    const output = input.map((row, pointer) => row[length] / row[pointer])
+    return output
+  }
 
+  // Scalar Multiple
+  static matrixScalarMultiply( matrix: number[][], scalar: number ) {
+    const input = matrix.slice()
+    const output = input.map((row) => row.map((cell) => cell * scalar))
+    return output
+  }
+
+  // Matrix Addition and Subtraction
+  static matrixAddition( matrix1: number[][], matrix2: number[][], addition:boolean) {
+    if (matrix1.length !== matrix2.length || matrix1[0].length !== matrix2[0].length) return 'Invalid Operation!'
+    const matrixA = matrix1.slice()
+    const matrixB = !addition ? this.matrixScalarMultiply(matrix2, -1) : matrix2.slice()
+
+    let output = []
+    for (let row = 0; row < matrix1.length; row += 1) {
+      let rowOutput = []
+      for (let col = 0; col < matrix1[0].length; col += 1) {
+        rowOutput.push(matrixA[row][col] + matrixB[row][col])
+      }
+    	output.push(rowOutput)
+    }
+    return output
+  }
+
+	// Matrix Transpose
+	static matrixTranspose( matrix: number[][] ) {
+		const input = matrix.slice()
+		let output = []
+		for (let col = 0; col < input[0].length; col += 1) {
+			let rowOutput = []
+			for (let row = 0; row < input.length; row += 1) {
+				rowOutput.push(input[row][col])
+			}
+			output.push(rowOutput)
+		}
+		return output
+	}
+
+  // Matrix Multiplication
+  static matrixMultiplication( matrix1: number[][], matrix2: number[][] ) {
+		if (matrix1[0].length !== matrix2.length) return 'Invalid Operation!'
+    const evaluateCell = ( row: number[], col: number[] ) => {
+			let output = 0
+      for (let i = 0; i < row.length; i += 1 ) {
+				output += (row[i] * col[i])
+			}
+			return output
+    }
+		const matrixA = matrix1.slice()
+		const matrixB = matrix2.slice()
+		let output = []
+		for (let col = 0; col < matrixB[0].length; col += 1) {
+			const colMultiply = matrixB.map((rowB) => rowB[col])
+			let rowOutput = []
+			for (let row = 0; row < matrixA.length; row += 1) {
+				rowOutput.push(evaluateCell(matrixA[row], colMultiply))
+			}
+			output.push(rowOutput)
+		}
+		return this.matrixTranspose(output)
+  }
+
+	// Matrix Trace
+	static matrixTrace( matrix: number[][] ) {
+		if (matrix.length !== matrix[0].length) return 'No Solution. The matrix do not have a main diagonal'
+		const output = matrix.reduce((total, pointer, index) => (total += pointer[index]), 0)
+		return output
+	}
 }
 
 export default ChapterOne;
