e51d3be2a830b9cb05f4fc0c7bb2fb8d0972d77b
Fritz <furitsucode@gmail.com>
2022-11-22 16:20:30 +0700

    Updated Chapter 3

--- src/materials/chapter3/chapter3.test.ts
+++ src/materials/chapter3/chapter3.test.ts
@@ -12,3 +12,35 @@ test('Vector Addition and Subtraction should give the correct solution', () => {
     [3,3,3]
   )
 })
+
+test('Vector Norm should return the length of the vector', () => {
+  expect(ChapterThree.vectorNorm([-3,2,1])).toStrictEqual(Math.sqrt(14))
+})
+
+test('Find the unit vector for a given vector', () => {
+  expect(ChapterThree.unitVector([2,2,-1])).toStrictEqual([2/3,2/3,-1/3])
+})
+
+test('Distance of two vector should return the proper scalar', () => {
+  expect(ChapterThree.twoVectorDistance(
+    [1,3,-2,7],[0,7,2,2]
+  )).toStrictEqual(Math.sqrt(58))
+})
+
+test('Dot product two vector should return the correct scalar', () => {
+  expect(ChapterThree.vectorDotProduct(
+    [-1,3,5,7],[-3,-4,1,0]
+  )).toStrictEqual(-4)
+})
+
+test('Angle calculation of two vector should return the correct angle', () => {
+  expect(ChapterThree.twoVectorAngle(
+    [2,0,0],[2,2,2]
+  )).toStrictEqual(Math.acos(1 / Math.sqrt(3)))
+})
+
+test('Should return the component of the first vector which is orthogonal to the second vector', () => {
+  expect(ChapterThree.vectorComponent(
+    [6,2],[3,-9]
+  )).toStrictEqual([6,2])
+})
--- src/materials/chapter3/chapter3.ts
+++ src/materials/chapter3/chapter3.ts
@@ -9,6 +9,56 @@ class ChapterThree {
     const output = vectorA.map((item, index) => item + vectorB[index])
     return output
   }
+
+  // Norm Vector
+  static vectorNorm( vector: number[] ) {
+    const input = vector.slice()
+    const output = Math.sqrt(input.reduce((total, current) => total += current * current, 0))
+    return output
+  }
+
+  // Unit Vector
+  static unitVector( vector: number[] ) {
+    const input = vector.slice()
+    const coef = this.vectorNorm(input)
+    const output = input.map((item) => item / coef)
+    return output
+  }
+
+  // Distance of two vectors
+  static twoVectorDistance( vector1: number[], vector2: number[] ) {
+    const vectorA = vector1.slice()
+    const vectorB = vector2.slice()
+    const output = Math.sqrt(vectorB.reduce((total, current, index) => total += Math.pow(current - vectorA[index], 2), 0))
+    return output
+  }
+
+  // Dot Product
+  static vectorDotProduct( vector1: number[], vector2: number[] ) {
+    const vectorA = vector1.slice()
+    const vectorB = vector2.slice()
+    const output = vectorB.reduce((total, current, index) => total += current * vectorA[index], 0)
+    return output
+  }
+
+  // Two vector angle degree
+  static twoVectorAngle( vector1: number[], vector2: number[] ) {
+    const vectorA = vector1.slice()
+    const vectorB = vector2.slice()
+    const output = Math.acos(this.vectorDotProduct(vectorA, vectorB) / (this.vectorNorm(vectorA) * this.vectorNorm(vectorB)))
+    return output
+  }
+
+  // Orthogonality
+  // Vector component
+  static vectorComponent( vector1: number[], vector2: number[] ) {
+    const vectorA = vector1.slice()
+    const vectorB = vector2.slice()
+    const scalar = this.vectorDotProduct(vectorA, vectorB) / Math.pow(this.vectorNorm(vectorB), 2)
+    const projAalongB = vectorB.map((unit) => unit * scalar)
+    const aOrthoToB = this.vectorAddition( vectorA, projAalongB, false)
+    return aOrthoToB
+  }
 }
 
 export default ChapterThree
